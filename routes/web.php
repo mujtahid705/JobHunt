ab91b4f36830cd8dc1ce81e6c92b84d34fd337a3
Muhammad Mujtahid <mujtahid705@gmail.com>
2024-08-30 17:18:28 +0600

    working on edit functionality

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,11 @@ Route::get('/', [ListingController::class, "index"]);
 // Create Listings
 Route::get("/listings/create", [ListingController::class, "create"]);
 
-// Post Listings
+// Stor Listing Data
 Route::post("/listings", [ListingController::class, "store"]);
+
+// Show Edit Form
+Route::get("/listings/{listing}/edit", [ListingController::class, "edit"]);
 
 // Single Listings
 Route::get("/listings/{listing}", [ListingController::class, "show"]);
